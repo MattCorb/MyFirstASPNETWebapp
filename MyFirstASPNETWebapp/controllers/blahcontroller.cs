76f6210c9d4f610f467cccd3801a7246ef6d1a63
MattCorb <32273733+MattCorb@users.noreply.github.com>
2022-01-13 16:19:52 -0700

    Mission 2 Prelim

---
+++ MyFirstASPNETWebapp/controllers/blahcontroller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASPNETWebapp.controllers
+{
+    public class blahcontroller : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+         
+            return View();
+        }
+
+        public IActionResult gradeCalc()
+        {
+            return View();
+        }
+
+    }
+}
